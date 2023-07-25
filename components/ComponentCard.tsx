0803d9d28a91a35f3b07e4994159e9460d92fb6e
Gautam Raj <ashragautam25@gmail.com>
2023-07-25 16:56:40 +0530

    deployment error #1

--- components/ComponentCard.tsx
+++ components/ComponentCard.tsx
@@ -1,42 +1,37 @@
-import Image from "next/image";
-import React from "react";
-
-import image1 from "../public/about/image1.png";
-
-const style = {
-  maxWidth: "100%",
-};
+import Image from 'next/image';
+import React from 'react';
 
 const arr = [
   {
-    src: "/about/image1.png",
-    title: "Platform for Thought Exchange",
+    src: '/about/image1.png',
+    title: 'Platform for Thought Exchange',
     para: "We provide a dynamic platform where individuals from diverse backgrounds can freely express their thoughts, opinions, and ideas. We  encourages open dialogue and fosters a community where everyone's voice is valued and respected.",
   },
   {
-    src: "/about/image2.png",
-    title: "Building Supportive Community",
+    src: '/about/image2.png',
+    title: 'Building Supportive Community',
     para: "Our website is more than just a platform for sharing thoughts; it's a community of like-minded individuals committed to social equality. Users can connect with others, form networks, and find support. More people more impact.",
   },
   {
-    src: "/about/image3.png",
-    title: "Amplifying Diverse Perspectives",
-    para: "We celebrate the richness of diverse perspectives and encourage users to share their unique experiences and insights. By amplifying underrepresented voices, we aim to foster empathy, understanding, and appreciation for different backgrounds.",
+    src: '/about/image3.png',
+    title: 'Amplifying Diverse Perspectives',
+    para: 'We celebrate the richness of diverse perspectives and encourage users to share their unique experiences and insights. By amplifying underrepresented voices, we aim to foster empathy, understanding, and appreciation for different backgrounds.',
   },
 ];
+
 const ComponentCard = () => {
   return (
     <div className="flex flex-row  items-center justify-center  flex-wrap mt-12 my-32">
       {arr.map((curr, i) => {
-        const color = `bg-[${curr.bgColor}]`;
         return (
           <div
-            className={`max-w-[300px] p-3 min-h-[450px] text-white rounded-lg ${color} mb-5 mx-3 ${
+            key={i}
+            className={`max-w-[300px] p-3 min-h-[450px] text-white rounded-lg  mb-5 mx-3 ${
               i === 2
-                ? "bg-[#8E31FF]"
+                ? 'bg-[#8E31FF]'
                 : i === 1
-                ? "bg-[#43D04D]"
-                : " bg-[#FC7201]"
+                ? 'bg-[#43D04D]'
+                : ' bg-[#FC7201]'
             }`}
           >
             <Image
